9ad3c0777708cce880fc81a3d983f6cb61570834
Michael Abed <michaelabed@gmail.com>
2013-10-18 00:56:25 -0400

    cuda paths

---
+++ roles/nvidia/files/cuda.sh
@@ -0,0 +1,6 @@
+# Cuda initialization
+
+export PATH=$PATH:/usr/local/cuda-5.5/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-5.5/lib64
+export INCLUDE=$INCLUDE:/usr/local/cuda-5.5/include
+
