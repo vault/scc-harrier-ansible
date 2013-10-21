e5cd8fa1597f483d5f28037801e803367fe8cdd1
Michael Abed <michaelabed@gmail.com>
2013-10-21 15:49:17 -0400

    nvidia ganglia stuff

--- roles/nvidia/files/cuda.sh
+++ roles/nvidia/files/cuda.sh
@@ -1,6 +1,6 @@
 # Cuda initialization
 
 export PATH=$PATH:/usr/local/cuda-5.5/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-5.5/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-5.5/lib64:/usr/lib64/nvidia
 export INCLUDE=$INCLUDE:/usr/local/cuda-5.5/include
 
